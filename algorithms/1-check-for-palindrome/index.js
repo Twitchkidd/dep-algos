af112b7e7aaa9fed2f55e10bb56d7a781eebe0d5
Gareth Field <field.gareth@gmail.com>
2021-09-18 05:01:53 -0400

    realization that getting long palindromes mean running a python program

--- algorithms/1-check-for-palindrome/index.js
+++ algorithms/1-check-for-palindrome/index.js
@@ -11,16 +11,22 @@ sometimes disregarding punctuation, capitalization and diacritics.
 // I just wonder if for really big strings whether comparing the chunks
 // might be faster than comparing by character.
 
-// Whoops, I committed again with the same message, I thought that was going to
-// run nodemon for some reason ¯\_(ツ)_/¯
+// Oh, creating palindromes that long is actually non-trivial, and this turns
+// into a longer exercise than I want to go through
 
 const isPalindrome = input => {
 	const regex = /[\W_]/g;
+	const ping = function () {
+		console.log(new Date().getTime());
+	};
 	const palindromeComparisonByCharacter = str => {
+		ping();
 		for (let i = 0; i < str.length / 2; i++) {
 			if (str[i] !== str[str.length - 1 - i]) {
+				ping();
 				return false;
 			}
+			ping();
 			return true;
 		}
 	};
@@ -49,6 +55,7 @@ const testInputs = [
 	'sad',
 	'racecar',
 	'abba',
+	'Are we not pure? “No sir!” Panama’s moody Noriega brags. “It is garbage!” Irony dooms a man; a prisoner up to new era.',
 	42385,
 	44244,
 	Number.POSITIVE_INFINITY,
